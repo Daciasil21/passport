489eea4b4939b7665779e460c56ae7c9506a38e0
Dacia Yang <yang.dacia@gmail.com>
2017-07-18 09:29:24 -0500

    password

--- app/models/user.js
+++ app/models/user.js
@@ -33,7 +33,7 @@ module.exports = function(sequelize, Sequelize) {
             }
         },
  
-        passwords: {
+        password: {
             type: Sequelize.STRING,
             allowNull: false
         },
